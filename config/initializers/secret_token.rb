1e45378890e387745201c88c5de24cb05858dde3
lotfy1893 <mohamed.lotfy.hammad@gmail.com>
2014-09-11 20:43:31 +0300

    implementing status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lotfybook::Application.config.secret_token = '3a79ae5b6b420826c2ad6ed28f1685151dc8908d9727ce2de08dbad30bfc5d824f147eae9266cd0283cb41321c20cdc52b173db62efd4d5d5396b71b03af79e4'
